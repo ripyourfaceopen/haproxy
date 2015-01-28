df8ce042df11c704591f8036bad045032703180d
chad <chad.chmura@dicecorp.com>
2015-01-28 01:20:13 -0500

    added container access to host for proper function of keepalived failover of haproxy

--- haproxy-start.sh
+++ haproxy-start.sh
@@ -5,7 +5,7 @@ if [ $1 == '--help' ]; then
 	exit 0
 fi
 
-docker run --name $1 -d -p 443:443 -v $PWD/:/etc/haproxy/ -v $PWD/kad$2:/etc/keepalived/ $3 haproxy -db -f /etc/haproxy/haproxy.cfg
+docker run --name $1 -d --net=host --privileged=true -v $PWD/:/etc/haproxy/ -v $PWD/kad$2:/etc/keepalived/ $3 haproxy -db -f /etc/haproxy/haproxy.cfg;keepalived
 
 if [ $? != 0 ]; then
 	echo "usage of this script: haproxy <name> <master or slave> <image>"
